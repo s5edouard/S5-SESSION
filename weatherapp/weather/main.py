4214279a35e8f84777c10034491c5fdb735a03e6
devopseasylearning <info@devopseasylearning.com>
2023-12-02 14:02:46 -0500

    changes

--- weatherapp/weather/main.py
+++ weatherapp/weather/main.py
@@ -10,17 +10,22 @@ CORS(app)
 def health():
     return "The service is running", 200
 
-@app.route('/<city>')
-def hello(city):
-    url = "https://weatherapi-com.p.rapidapi.com/current.json"
-    querystring = {"q":city}
-    headers = {
-        'x-rapidapi-host': "weatherapi-com.p.rapidapi.com",
-        'x-rapidapi-key': os.getenv("APIKEY")
-    }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    return jsonify(response.text)
+@app.route('/weather/<city>')
+def get_weather(city):
+    api_key = os.getenv("ecbc396f46mshb65cbb1f82cf334p1fcc87jsna5e962a3c542")
+    if not api_key:
+        return "OpenWeatherMap API key is missing", 500
 
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {"q": city, "appid": api_key, "units": "metric"}
+    
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        weather_data = response.json()
+        return jsonify(weather_data)
+    except requests.exceptions.RequestException as e:
+        return str(e), 500
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
